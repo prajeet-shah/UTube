ec69a9fbb8366001c97e335e27e4fbda0b6b49aa
Prajeet Shah <prajeetshah93@gmail.com>
2025-03-01 10:50:05 +0545

    nested level comment features

---
+++ src/components/CommentsContainer.js
@@ -0,0 +1,137 @@
+import React from "react";
+
+const commentData = [
+  {
+    name: "prajeet shah",
+    comment: "lorem is the best the best of all time",
+    replies: [],
+  },
+  {
+    name: "prajeet shah",
+    comment: "lorem is the best the best of all time",
+    replies: [
+      {
+        name: "prajeet shah",
+        comment: "lorem is the best the best of all time",
+        replies: [],
+      },
+      {
+        name: "prajeet shah",
+        comment: "lorem is the best the best of all time",
+        replies: [],
+      },
+      {
+        name: "prajeet shah",
+        comment: "lorem is the best the best of all time",
+        replies: [
+          {
+            name: "prajeet shah",
+            comment: "lorem is the best the best of all time",
+            replies: [],
+          },
+          {
+            name: "prajeet shah",
+            comment: "lorem is the best the best of all time",
+            replies: [
+              {
+                name: "prajeet shah",
+                comment: "lorem is the best the best of all time",
+                replies: [],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "prajeet shah",
+    comment: "lorem is the best the best of all time",
+    replies: [],
+  },
+  {
+    name: "prajeet shah",
+    comment: "lorem is the best the best of all time",
+    replies: [
+      {
+        name: "prajeet shah",
+        comment: "lorem is the best the best of all time",
+        replies: [
+          {
+            name: "prajeet shah",
+            comment: "lorem is the best the best of all time",
+            replies: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "prajeet shah",
+    comment: "lorem is the best the best of all time",
+    replies: [
+      {
+        name: "prajeet shah",
+        comment: "lorem is the best the best of all time",
+        replies: [
+          {
+            name: "prajeet shah",
+            comment: "lorem is the best the best of all time",
+            replies: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "prajeet shah",
+    comment: "lorem is the best the best of all time",
+    replies: [],
+  },
+];
+
+const Comment = ({ data }) => {
+  const { name, comment, replies } = data;
+  return (
+    <div className="flex my-2">
+      <img
+        className="w-12 h-12 mx-1"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtRs_rWILOMx5-v3aXwJu7LWUhnPceiKvvDg&s"
+        alt="user-icon"
+      />
+      <div className="px-3 bg-gray-100 w-full shadow-sm rounded-lg">
+        <p className="font-bold">{name}</p>
+        <p>{comment}</p>
+      </div>
+    </div>
+  );
+};
+
+const CommentList = ({ comments }) => {
+  console.log(comments);
+  return (
+    <div>
+      {comments.map((comment, index) => (
+        <div>
+          <Comment key={index} data={comment} />
+          <div className="mx-10 border border-l-black border-r-0 border-t-0 border-b-0">
+            <CommentList comments={comment.replies} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const CommentsContainer = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-3xl p-2 m-4">Comments: </h1>
+      <div>
+        <CommentList comments={commentData} />
+      </div>
+    </div>
+  );
+};
+
+export default CommentsContainer;
--- src/components/WatchVideo.js
+++ src/components/WatchVideo.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router";
 import { closeMenu } from "../utils/appSlice";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchVideo = () => {
   const dispatch = useDispatch();
@@ -13,17 +14,23 @@ const WatchVideo = () => {
   console.log(searchParams.get("v"));
 
   return (
-    <div>
-      <iframe
-        width="1200"
-        height="600"
-        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-      ></iframe>
+    <div className="p-5">
+      <div>
+        <iframe
+          width="1200"
+          height="600"
+          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
+
+      <div>
+        <CommentsContainer />
+      </div>
     </div>
   );
 };
