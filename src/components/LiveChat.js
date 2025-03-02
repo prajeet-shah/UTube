664b831b302268380857d0b047eec081c3d78ab3
Prajeet Shah <prajeetshah93@gmail.com>
2025-03-03 00:19:39 +0545

    adding chat message in Live Chat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
 import { generateRandomName, getRandomMessage } from "../utils/helper";
 
 const LiveChat = () => {
+  const [LiveChatmessage, setLiveChatMessage] = useState();
   const dispatch = useDispatch();
 
   const chatMessage = useSelector((store) => store.chat.message);
@@ -24,11 +25,37 @@ const LiveChat = () => {
   }, []);
 
   return (
-    <div className="px-2 mx-2 border border-black w-[450px] h-[700px] rounded-lg bg-slate-100 overflow-y-scroll flex flex-col-reverse">
-      {chatMessage.map((c, i) => (
-        <ChatMessage key={i} name={c.name} message={c.message} />
-      ))}
-      <ChatMessage name="prajeet shah" message="This is live chat" />
+    <div>
+      <div className="px-2 mx-2 border border-black w-[450px] h-[700px] rounded-lg bg-slate-100 overflow-y-scroll flex flex-col-reverse">
+        {chatMessage.map((c, i) => (
+          <ChatMessage key={i} name={c.name} message={c.message} />
+        ))}
+      </div>
+      <div>
+        <form
+          className="w-full border border-black bg-gray-100 h-10 rounded-lg"
+          onSubmit={(e) => {
+            e.preventDefault();
+            dispatch(
+              addMessage({
+                name: "prajeet shah",
+                message: LiveChatmessage,
+              })
+            );
+          }}
+        >
+          <input
+            className="w-3/4 h-9 rounded-lg bg-gray-200 px-2 text-lg border-none focus:outline-none"
+            type="text"
+            placeholder="chat here"
+            value={LiveChatmessage}
+            onChange={(e) => setLiveChatMessage(e.target.value)}
+          />
+          <button className="bg-red-600 text-white px-5 py-1 text-md rounded-lg ">
+            submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/vide
 export const YOUTUBE_SEARCH_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
-export const OFFSET_LIVE_CHAT = 20;
+export const OFFSET_LIVE_CHAT = 50;
 
 export const USER_ICON =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtRs_rWILOMx5-v3aXwJu7LWUhnPceiKvvDg&s";
